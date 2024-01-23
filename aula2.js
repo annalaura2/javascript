c26bea4c6df610e726bf567516154f96d5030a98
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-23 14:54:44 -0300

    Update aula2.js

---
+++ aula2.js
@@ -0,0 +1,13 @@
+// tipos de operadores utilizando javascript 
+
+let adicao = 10 + 10; 
+console.log("Adição: " + adicao);
+
+let subtracao = 56 - 40; 
+console.log ("Subtração: " + subtracao); 
+
+let multi = 10*9; 
+console.log("Multiplicação: " + multi); 
+
+let divisao = 30/3; 
+console.log("Divisão: " + divisao);
