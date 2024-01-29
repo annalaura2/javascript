fb723e16d0fcb53fabba1ece994a428e6473d294
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-29 10:34:15 -0300

    Add files via upload

---
+++ estrRep.js
@@ -0,0 +1,12 @@
+const notas = [];
+
+notas.push(5); 
+notas.push(7); 
+notas.push(8);
+
+console.log(notas.length); // mostra o tamanho da lista
+
+// estrutura de repetição for 
+for (let i = 0; i < 10 ; i++) {
+    console.log(i);
+}
