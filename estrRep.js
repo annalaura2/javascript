fe4a0cdc344e13da725b655b910cc1470c3e8df3
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-29 10:54:09 -0300

    Update estrRep.js

--- estrRep.js
+++ estrRep.js
@@ -1,12 +1,17 @@
 const notas = [];
 
-notas.push(5); 
-notas.push(7); 
+notas.push(5);
+notas.push(10);
 notas.push(8);
 
-console.log(notas.length); // mostra o tamanho da lista
+// notas.lengt = mostra o tamanho da lista
 
-// estrutura de repetição for 
-for (let i = 0; i < 10 ; i++) {
-    console.log(i);
+let soma = 0;
+
+for (i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
 }
+
+const media = soma / notas.length;
+console.log(media.toFixed(2));
